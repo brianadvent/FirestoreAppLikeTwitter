a91ffccae1d8114df383faff90f307a87ddab6a9
Brian Advent <youtube@brianadvent.com>
2017-10-02 18:57:10 +0200

    Initial Commit

---
+++ firestoreapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firestoreapp
+//
+//  Created by Brian Advent on 02.10.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
